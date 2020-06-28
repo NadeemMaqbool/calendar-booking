4b6aefa0c42178ce5e4eeb96b5f4f7089fb53ed7
Muhammad Nadeem <nd.maqbool@gmail.com>
2020-06-28 17:08:27 +0500

    Calendar App first commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,12 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { BookingFormComponent } from './booking-form/booking-form.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    BookingFormComponent
   ],
   imports: [
     BrowserModule
